2fa78f97b8e8be335b6c6ac92d9f55bbf4e2ac14
tymino <d_team-lex@bk.ru>
2022-06-22 16:45:23 +0300

    template, buttons

--- src/components/Slider/Slider.tsx
+++ src/components/Slider/Slider.tsx
@@ -1,5 +1,5 @@
 import './Slider.sass';
-import { FC, useState } from 'react';
+import { FC, useState, MouseEvent } from 'react';
 
 interface SliderProps {
   images: string[];
@@ -8,15 +8,58 @@ interface SliderProps {
 const Slider: FC<SliderProps> = ({ images }) => {
   const [item] = useState<number[]>([1, 2, 3]);
 
+  const [offset, setOffset] = useState(0);
+
+  const setStyleWidth = () => {
+    return {
+      width: '450px',
+    };
+  };
+
+  const handleClick = (e: MouseEvent<HTMLButtonElement>) => {
+    if (e.currentTarget.name === 'prev') {
+      setOffset((currentOffset) => {
+        const newOffset = currentOffset + 450;
+
+        return Math.min(newOffset, 0);
+      });
+    } else {
+      setOffset((currentOffset) => {
+        const newOffset = currentOffset - 450;
+        const maxOffset = -((item.length - 1) * 450);
+
+        return Math.max(newOffset, maxOffset);
+      });
+    }
+  };
+
   return (
-    <div className="slider__container">
+    <div className="slider__container" style={setStyleWidth()}>
+      <button
+        className="slider__button slider__button--prev"
+        name="prev"
+        onClick={handleClick}>
+        {'<'}
+      </button>
+
       <div className="slider__window">
-        <div className="slider__items">
+        <div
+          className="slider__items"
+          style={{ transform: `translateX(${offset}px)` }}>
           {item.map((item) => (
-            <div className={`item item__${item}`}>{item}</div>
+            <div className={`item item__${item}`} key={item}>
+              {item}
+            </div>
           ))}
         </div>
       </div>
+
+      <button
+        className="slider__button slider__button--next"
+        name="next"
+        onClick={handleClick}>
+        {'>'}
+      </button>
     </div>
   );
 };
