ba46c780542d6943cccbf963fa8fd5a50dbe2205
tymino <d_team-lex@bk.ru>
2022-06-22 15:56:06 +0300

    template

--- src/components/Slider/Slider.tsx
+++ src/components/Slider/Slider.tsx
@@ -1,12 +1,24 @@
 import './Slider.sass';
-import { FC } from 'react';
+import { FC, useState } from 'react';
 
 interface SliderProps {
   images: string[];
 }
 
 const Slider: FC<SliderProps> = ({ images }) => {
-  return <div className='slider__container'>slider</div>;
+  const [item] = useState<number[]>([1, 2, 3]);
+
+  return (
+    <div className="slider__container">
+      <div className="slider__window">
+        <div className="slider__items">
+          {item.map((item) => (
+            <div className={`item item__${item}`}>{item}</div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Slider;
